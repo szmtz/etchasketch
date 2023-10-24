dbb43f8acf6c070107a88f75a3f9b47d4a122b09
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-24 15:33:34 +0000

    Add/Finish click to Draw button actions

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 
 let color = "black"; 
+let click = false; 
 
 
 document.addEventListener("DOMContentLoaded", function() {
     createBoard(16);
+
+    document.querySelector("body").addEventListener("click", function(e){
+        if(e.target.tagName != "BUTTON") {
+            click = !click; 
+            let draw = document.querySelector("#draw"); 
+            if(click){
+                draw.innerHTML = "You are allowed to draw!"; 
+            }
+            else {
+                draw.innerHTML = "You are not allowed to draw."; 
+            }
+
+        }
+
+    })
+
     let btn_popup = document.querySelector("#popup"); 
     btn_popup.addEventListener("click", function(){
         let size = getSize(); 
@@ -44,11 +61,13 @@ function getSize() {
 }
 
 function colorDiv() {
-    if(color == "random"){
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-    }
-    else {
-        this.style.backgroundColor = 'black'; 
+    if(click){
+        if(color == "random"){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        }
+        else {
+            this.style.backgroundColor = 'black'; 
+        }
     }
 }
 
